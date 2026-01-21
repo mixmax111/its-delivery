17acb6147995276082e8a13266060abe6fe56084
mixmax111 <antoninodanna11@gmail.com>
2026-01-21 20:02:31 +0100

    Ripristino URL diretto Heroku

--- frontend/src/graphql/client.js
+++ frontend/src/graphql/client.js
@@ -2,15 +2,16 @@ import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-    uri: '/api/graphql',
+
+    uri: 'https://adanna-sja34.herokuapp.com/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
-    const token = localStorage.getItem('auth_token');
+    const token = localStorage.getItem('token');
     return {
         headers: {
             ...headers,
-            authorization: token ? `Basic ${token}` : "",
+            authorization: token ? `Bearer ${token}` : '',
         }
     }
 });
