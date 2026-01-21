650b0ec79013252fca78878b36bbdb99930bb882
mixmax111 <antoninodanna11@gmail.com>
2026-01-21 21:16:00 +0100

    AuthContext completo con Login Reale

--- frontend/src/graphql/client.js
+++ frontend/src/graphql/client.js
@@ -2,11 +2,11 @@ import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
+    // Usa il tuo link backend aggiornato
     uri: 'https://adanna-sja34-87786fd1c68b.herokuapp.com/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
-    // CORREZIONE QUI: Usiamo 'auth_token' invece di 'token'
     const token = localStorage.getItem('auth_token');
 
     return {
